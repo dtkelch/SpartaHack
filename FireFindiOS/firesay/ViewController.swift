f0c26f5dfe5f735d63cb349e1ed3bb71ea0aafcb
Daniel Kelch <dtkelch@gmail.com>
2016-02-27 12:42:38 -0500

    Working photo viewer for iOS.

--- FireFindiOS/firesay/ViewController.swift
+++ FireFindiOS/firesay/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     
+    @IBOutlet var imageView: UIImageView!
     @IBOutlet weak var text: UITextField!
+//    var imagePicker: UIImagePickerController!
+    var imagePicker = UIImagePickerController()
+
     
     var myRootRef:Firebase!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+
         // Create a reference to a Firebase location
         myRootRef = Firebase(url:"https://incandescent-fire-2307.firebaseio.com")
         // Write data to Firebase
@@ -33,8 +39,25 @@ class ViewController: UIViewController {
     
     @IBAction func buttonClicked(sender: AnyObject) {
         
-        myRootRef.setValue(text.text)
+        //  myRootRef.setValue(text.text)
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
+    
+func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .ScaleAspectFit
+            imageView.image = pickedImage
+        let unwrappedImage = pickedImage
+        let data = UIImageJPEGRepresentation(unwrappedImage, 0.1) // 0.0 to 1.0 - sets jpeg quality
+        print(data)
+    }
+    
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
