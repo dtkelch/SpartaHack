c60130a44916b5f95df27d9cc335f7dbac5a4035
Spencer Amann <amanns@mail.gvsu.edu>
2016-02-27 01:54:29 -0500

    fix gradle add firebase

--- app/src/main/java/gvsu/firesay/MainActivity.java
+++ app/src/main/java/gvsu/firesay/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Firebase.setAndroidContext(this);
     }
 
     @Override
