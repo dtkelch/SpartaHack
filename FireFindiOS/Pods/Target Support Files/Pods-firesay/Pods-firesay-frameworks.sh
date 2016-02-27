6aba36d1937a0e05333d850dc41fb0bf54fb915c
Daniel Kelch <dtkelch@gmail.com>
2016-02-27 13:31:24 -0500

    Clarifai integration.

--- FireFindiOS/Pods/Target Support Files/Pods-firesay/Pods-firesay-frameworks.sh
+++ FireFindiOS/Pods/Target Support Files/Pods-firesay/Pods-firesay-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-firesay/AFNetworking.framework"
   install_framework "Pods-firesay/Firebase.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-firesay/AFNetworking.framework"
   install_framework "Pods-firesay/Firebase.framework"
 fi
--- FireFindiOS/firesay/ViewController.swift
+++ FireFindiOS/firesay/ViewController.swift
@@ -10,20 +10,20 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
     
-    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var text: UITextField!
-//    var imagePicker: UIImagePickerController!
-    var imagePicker = UIImagePickerController()
-
+    //    var imagePicker: UIImagePickerController!
     
     var myRootRef:Firebase!
+    private lazy var client : ClarifaiClient = ClarifaiClient(appID: clarifaiClientID, appSecret: clarifaiClientSecret)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imagePicker.delegate = self
-
+        
         // Create a reference to a Firebase location
         myRootRef = Firebase(url:"https://incandescent-fire-2307.firebaseio.com")
         // Write data to Firebase
@@ -39,31 +39,71 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     @IBAction func buttonClicked(sender: AnyObject) {
         
-        //  myRootRef.setValue(text.text)
-        imagePicker.sourceType = .PhotoLibrary
+        // Show a UIImagePickerController to let the user pick an image from their library.
+        let picker = UIImagePickerController()
+        picker.sourceType = .PhotoLibrary
+        picker.allowsEditing = false
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
         
-        presentViewController(imagePicker, animated: true, completion: nil)
+        
     }
     
-func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-    if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.contentMode = .ScaleAspectFit
+            textView.text = "Recognizing..."
+            button.enabled = false
             imageView.image = pickedImage
-        let unwrappedImage = pickedImage
-        let data = UIImageJPEGRepresentation(unwrappedImage, 0.1) // 0.0 to 1.0 - sets jpeg quality
-        print(data)
+            recognizeImage(pickedImage)
+
+            //let unwrappedImage = pickedImage
+            //let data = UIImageJPEGRepresentation(unwrappedImage, 0.1) // 0.0 to 1.0 - sets jpeg quality
+            //print(data)
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+
     }
     
-        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+    private func recognizeImage(image: UIImage!) {
+        // Scale down the image. This step is optional. However, sending large images over the
+        // network is slow and does not significantly improve recognition performance.
+        let size = CGSizeMake(320, 320 * image.size.height / image.size.width)
+        UIGraphicsBeginImageContext(size)
+        image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // Encode as a JPEG.
+        let jpeg = UIImageJPEGRepresentation(scaledImage, 0.9)!
+        
+        // Send the JPEG to Clarifai for standard image tagging.
+        client.recognizeJpegs([jpeg]) {
+            (results: [ClarifaiResult]?, error: NSError?) in
+            if error != nil {
+                print("Error: \(error)\n")
+                self.textView.text = "Sorry, there was an error recognizing your image."
+            } else {
+                print(results![0].tags.joinWithSeparator(", "))
+                self.textView.text = "Tags:\n" + results![0].tags.joinWithSeparator(", ")
+            }
+            self.button.enabled = true
+        }
     }
+
     
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
